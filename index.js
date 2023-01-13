c4d0cec0f949a483b223499384ce52e2cd20841a
TsukiHyaku <alexander.lamas.2003@gmail.com>
2023-01-12 23:23:11 -0300

    filtrados

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-class ProductManager {
-    constructor() {
-        this.products = []
+class ProductManager{
+    constructor( ) {
+        this.products = [ ]
     }
-
-    getproducts = () => this.products
-    addProduct = (img ,title, descripcion, precio, capacidad) => {
-        const product = {
-            img,
-            title,
-            descripcion,
-            precio,
-            capacidad
+    getProducts = ( ) => this.products
+    getProductById = ( id ) => { 
+        const productDb = this.products.find (product =>product.id === id)
+        if (!productDb) {
+            return `Producto no Encontrado  con el id: ${id}` 
+        }
+    }
+    addProduct =(newProduct) =>{
+        const productDb = this.products.find (product =>product.code === newProduct.code)
+        if (productDb){
+        return  `Producto Encontrado` 
         }
 
         if (this.products.length === 0) {
-            product.id = 1
+            newProduct.id = 1
+            this.products.push(newProduct)
         } else {
-            
-            product.id = this.products[this.products.length - 1].id + 1
-            
+            this.products = [...this.products, {...newProduct, id: this.products[this.products.length-1].id+1 }]
         }
-        this.products.push(product) 
-        console.log(this.products)
-    } 
-     
+    }
 }
 
-const productManager = new ProductManager()
-productManager.addProduct('...', 'KissxSis', 'KissxSis es un manga de Romance','$3500',200)
+const productos = new ProductManager( )
+console.log(productos.addProduct({
+    title: 'Domestic No Kanojo',
+    description:'Manga de Romance y Drama',
+    price: 500,
+    img: '...',
+    code:1,
+    stock:200
+}))
 
+console.log(productos.addProduct({
+    title: 'Konosuba',
+    description:'Manga Isekai y Comedia',
+    price: 500,
+    img: '...',
+    code:2,
+    stock:100
+}))
+
+console.log(productos.getProducts())
+console.log(productos.getProductById(3))
