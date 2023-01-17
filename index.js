1ff008968b650e2b3277cefe05b834231108ecd3
TsukiHyaku <alexander.lamas.2003@gmail.com>
2023-01-17 18:53:53 -0300

    validacion de capos

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ class ProductManager{
     }
     addProduct =(newProduct) =>{
         const productDb = this.products.find (product =>product.code === newProduct.code)
+        
         if (productDb){
         return  `Producto Encontrado` 
         }
+
+        if (newProduct.title === '' || newProduct.description === '' || newProduct.price === '' || newProduct.thumbnail === '' || newProduct.code === '' || newProduct.stock === '')  {
+            return `Debe completar todos los campos `
+          }
 
         if (this.products.length === 0) {
             newProduct.id = 1
@@ -54,13 +59,20 @@ console.log(productos.addProduct({
 }))
 
 console.log(productos.addProduct({
-    title: 'Aku no Hana',
-    description:'Manga de Historia de Vida',
+    title: '',
+    description:'',
     price: 500,
     img: '...',
     code:4,
     stock:100
 }))
+console.log(productos.addProduct({
+    title: '',
+    description:'Manga de Historia de Vida',
+    price: 500,
+    img: '...',
+    stock:100,
+}))
 
 console.log(productos.getProducts())
-console.log(productos.getProductById(5))
+console.log(productos.getProductById(6))
