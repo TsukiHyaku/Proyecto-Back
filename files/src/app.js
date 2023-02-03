f96d93654fb252387625a162d42ab2b9afc5010c
~Yuki~ <105572109+TsukiHyaku@users.noreply.github.com>
2023-02-03 00:02:33 -0300

    PRIMERA ENTREGA DEL PROYECTO FINAL

--- files/src/app.js
+++ files/src/app.js
@@ -1,36 +1,64 @@
 import express from 'express'
-import { ProductManager } from './index.js'
+import productsRouter from './routes/products.js'
+import cartsRouter from './routes/carts.js'
 
-const productos = new ProductManager('./files/productos.json')
-const app = express()
+const app = express ()
 const PORT = 8080
 
+app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.get("/products", async (req, res) => {
-  try {
-    const { limit } = req.query;
-    const data = await productos.getProducts();
-    const products = data.slice(0, limit);
-    res.send(products);
-  } catch (error) {
-    res.send(error);
-  }
-});
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
 
-app.get("/products/:pId", async (req, res) => {
-  const pid = req.params.pId;
-  if (!pid) {
-    return res.status(400).json({ error: "id no valido" });
-  }
-  const product = await productos.getProductById();
-  if (!product) {
-    return res.status(404).json({ error: "Producto no encontrado" });
-  }
-  return res.send(product);
-});
-
-app.listen(PORT, err => {
-  if (err) console.log(err)
-  console.log(`Escuchando en el puerto ${PORT}`)
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log('Escuchando puerto: ', PORT);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { ProductManager } from './productManager.js'
+
+// const productos = new ProductManager('./files/productos.json')
+// const app = express()
+// const PORT = 8080
+
+// app.use(express.urlencoded({extended:true}))
+
+// app.get('/products', async (req, res) => {
+//   const { limit } = req.query
+//   try {
+//     const data = await productos.getProducts()
+//     limit ? res.send(data.filter(product => product.id <= limit)) : res.send(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
+
+// app.get("/products/:pid", async (req, res)=>{
+//   const pid = req.params.pid
+//   try {
+//       const data = await productos.getProducts()
+
+//       pid ? res.send(data.find(product => product.id.toString() ===  pid)) : res.send(data)
+//   } catch (error) {
+//       console.log(error)
+//   }
+// })
+
+// app.listen(PORT, err => {
+//   if (err) console.log(err)
+//   console.log(`Escuchando en el puerto ${PORT}`)
+// })
